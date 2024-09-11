8907fd88ef58b04e4fc49506bd5b6269ce8d9d1f
aaron.fernandez <aaron.fernandez@alisys.net>
2024-09-11 11:23:54 +0200

    create error component and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {CreateComponent} from './components/create/create.component';
+import { ProjectsComponent} from './components/projects/projects.component';
+import { ErrorComponent} from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AboutComponent},
+    {path: 'sobre-mi', component: AboutComponent},
+    {path: 'contacto', component: ContactComponent},
+    {path: 'crear-proyecto', component: CreateComponent},
+    {path: 'proyectos', component: ProjectsComponent},
+    {path: '**', component: ErrorComponent}
+];
+
