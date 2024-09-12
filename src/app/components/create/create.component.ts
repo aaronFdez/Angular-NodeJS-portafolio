ef31541e5c7e7851ce0ca7c84677dcfb21c6344e
aaron.fernandez <aaron.fernandez@alisys.net>
2024-09-12 11:37:42 +0200

    upload images in form

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { Project } from "../../models/project";
-import { ProjectService } from "../../services/project.service";
-import { FormsModule, NgForm } from "@angular/forms";
-import { NgIf } from "@angular/common";
-import { ProjectResponse } from "../../models/project-response";
+import {Component} from '@angular/core';
+import {Project} from "../../models/project";
+import {ProjectService} from "../../services/project.service";
+import {UploadService} from "../../services/upload.service";
+import {FormsModule, NgForm} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {ProjectResponse} from "../../models/project-response";
 import {RouterLink} from "@angular/router";
+import {global} from "../../services/global";
 
 @Component({
   selector: 'app-create',
@@ -23,22 +25,37 @@ export class CreateComponent {
   public project: Project;
   public status: string;
   public currentYear = new Date().getFullYear();
+  public filesToUpload: Array<File>;
 
-  constructor(private _projectService: ProjectService) {
+  constructor(private _projectService: ProjectService, private _uploadService: UploadService) {
     this.title = "Crear proyecto";
     this.project = new Project('', '', '', '', this.currentYear, '', '');
     this.status = 'pending';
+    this.filesToUpload = [];
   }
 
   onSubmit(projectForm: NgForm) {
     console.log(this.project);
     this._projectService.saveProject(this.project).subscribe(
-      (response: ProjectResponse) => {
-        console.log(response);
+      (response: any) => {  // Cambié 'ProjectResponse' a 'any' para manejar cualquier respuesta
         if (response.project) {
-          this.status = 'success';
-          this.project = response.project;
-          projectForm.reset();
+          if (this.filesToUpload && this.filesToUpload.length > 0) {
+            // Subir imágenes solo si hay archivos
+            this._uploadService.makeFileRequest(global.url + 'upload-image/' + response.project._id, [], this.filesToUpload, 'image')
+              .then((result: any) => {
+                console.log(result);
+                this.status = 'success';
+                projectForm.reset();
+              })
+              .catch((error: any) => {
+                console.error('Error uploading files:', error);
+                this.status = 'failed';
+              });
+          } else {
+            // Si no hay archivos, simplemente marca como éxito
+            this.status = 'success';
+            projectForm.reset();
+          }
         } else {
           this.status = 'failed';
         }
@@ -47,5 +64,11 @@ export class CreateComponent {
         console.log(<any>error);
       }
     );
+  }
+
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+
   }
 }
